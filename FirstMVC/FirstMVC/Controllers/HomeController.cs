9ba0a1c83c5840dd42a63f4216e56fcff7140466
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-09-12 13:07:17 +0530

    initial file

---
+++ FirstMVC/FirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVC.Models; 
+
+namespace FirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index()
+        {
+            return "Hello, This is my first MVC application !!!";
+        }
+
+        public string Greet(string name)
+        {
+            return string.Format("Hello {0}, this is my first ASP.NET MVC application", "John"); 
+        }
+
+
+        public ActionResult ShowDetails(string name)
+        {
+            ViewBag.Message = "Hello Rajan";
+            ViewBag.YourID = "10";
+
+            return View(); 
+        }
+
+        public ActionResult CustomerDetails()
+        {
+            Customer cust = new Customer();
+            cust.CustomerID = "ALFKI";
+            cust.CompanyName = "Alfreds Futterkiste";
+            cust.Age = 20;
+            cust.JoinDate = DateTime.Now.AddYears(-4);
+            cust.Email = "abc@amail.com";
+            cust.CustomerType = EnumCustomerType.New; 
+
+            return View(cust);
+        }
+
+
+        [HttpPost]
+        public ActionResult CustomerDetails(Customer cust)
+        {
+            
+            //code to update Customer details in database
+            return View();
+        }
+    }
+}
---
+++ FirstMVC/FirstMVC/Models/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;  
+
+namespace FirstMVC.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage="Customer ID is required")]        
+        public string CustomerID { get; set; }
+
+        [StringLength(10, ErrorMessage = "Company Name should be less than or equal to ten characters.")]        
+        public string CompanyName { get; set; }
+
+        [Range(20,40, ErrorMessage="Customer Age should be in 20 to 40 range." )]   
+        public int Age { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime JoinDate { get; set; }
+        
+        [DataType(DataType.EmailAddress,ErrorMessage="Email is not valid." )]  
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$",ErrorMessage="Email is not valid.")]  
+        public string Email { get; set; }        
+        
+        [EnumDataType
+            (typeof(EnumCustomerType))]  
+        public EnumCustomerType CustomerType { get; set; }
+    }
+
+    public enum EnumCustomerType
+    {
+        New = 0,
+        Regular = 1,
+        Elite = 2
+    }
+}
